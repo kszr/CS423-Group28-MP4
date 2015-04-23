8b08fa22fb89a9b6c11c51a0b8b8d5518bc26f49
kszr <kszr@users.noreply.github.com>
2015-04-23 17:24:46 -0500

    Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by abhishekchatterjee on 4/23/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world...");
+    }
+}
