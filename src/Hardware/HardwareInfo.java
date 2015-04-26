87ebc1dafed4cb1e5ad301023d6e6ce06b93d7ca
kszr <kszr@users.noreply.github.com>
2015-04-26 17:22:07 -0500

    Adding GUI and Hardware information.

---
+++ src/Hardware/HardwareInfo.java
@@ -0,0 +1,10 @@
+package Hardware;
+
+/**
+ * Created by abhishekchatterjee on 4/26/15.
+ *
+ * Stores hardware information, which includes CPU utilization.
+ */
+public class HardwareInfo {
+    public double cpuUtil;
+}
