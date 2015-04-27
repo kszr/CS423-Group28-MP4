53d3e3beba50c62a22e548c39321908ff9473663
Matthew Rasmussen <matthewerikrasmussen@gmail.com>
2015-04-27 00:42:44 -0500

    Created setup spaghetti in Adapter

--- src/Adapter/Adapter.java
+++ src/Adapter/Adapter.java
@@ -1,30 +1,85 @@
 package Adapter;
 
+import Aggregation.AggregatorSender;
+import Aggregation.JobAggregator;
+import Balancer.JobRequester;
+import Balancer.JobServer;
+import Balancer.QueueWatcher;
 import Hardware.HardwareMonitor;
+import Jobs.Job;
+import Jobs.JobQueue;
+import Jobs.JobSplitter;
 import State.StateManager;
+import Worker.WorkerManager;
 
 /**
  * Created by abhishekchatterjee on 4/26/15.
  */
 public class Adapter {
-    private HardwareMonitor hardwareMonitor;
-    private StateManager stateManager;
 
-    int portNumber;
-    int jobCount;
+    // Set these before calling Setup()
+    public int numElements;
+    public int numJobs;
+
+    public int numWorkerThreads;
+
+    public int ownJobServerPort;
+
+    public String otherAddress;
+    public int otherJobServerPort;
+
+    public boolean isAggregator;
+    public String aggregatorAddress;
+    public int aggregatorPort;
+
+
+    // Created during Setup();
+    private JobQueue jobQueue;
+
+    private JobRequester jobRequester;
+    private JobServer jobServer;
+
+    private WorkerManager workerManager;
+
+    private QueueWatcher queueWatcher;
+
+    private HardwareMonitor hardwareMonitor;
+
+    private AggregatorSender aggregatorSender;
+    private JobAggregator jobAggregator; // null iff isAggregator == false
+
+
 
     public Adapter() {
-        hardwareMonitor = new HardwareMonitor();
 
-        //initialize portNumber to something.
-        portNumber = 0;
 
-        stateManager = new StateManager(portNumber);
     }
 
-    private void sendState() {
-        stateManager.updateState(jobCount, hardwareMonitor.getThrottlingValue(), hardwareMonitor.getHardwareInfo());
+    public void setup() {
+        jobQueue = new JobQueue();
+
+        jobServer = new JobServer(ownJobServerPort, jobQueue);
+        jobRequester = new JobRequester(otherAddress, otherJobServerPort, jobQueue);
+
+        if (isAggregator) {
+            jobAggregator = new JobAggregator(aggregatorPort, numJobs);
+        }
+        aggregatorSender = new AggregatorSender(aggregatorAddress, aggregatorPort);
+
+        workerManager = new WorkerManager(jobQueue, aggregatorSender, numWorkerThreads);
+
+        hardwareMonitor = new HardwareMonitor(workerManager);
+
+        JobSplitter splitter = new JobSplitter(numElements, numJobs);
+        for (Job job : splitter) {
+            jobQueue.addJob(job);
+        }
+
+        queueWatcher = new QueueWatcher(jobQueue, jobRequester);
+
+
     }
+
 
     public void openHardwareMonitorInterface() {
         hardwareMonitor.openInterface();
